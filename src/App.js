5a71bc6b81fe779ba86fa552562cd413c3083aae
Ridhima-s <ridhima7193.ca22@chitkara.edu.in>
2024-03-26 23:55:42 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Sign-in" element={<Signin />} />
+      <Route path="/Sign-up" element={<Signup />} />
+      <Route path="/Forgot-Password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
